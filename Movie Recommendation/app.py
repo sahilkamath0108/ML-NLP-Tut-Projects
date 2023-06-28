59b54e2e84e8e68d73e574da456e39c34d717612
Sahil Kamath <sahilkamath0108@gmail.com>
2023-06-28 17:12:10 +0530

    movie recommendation system

---
+++ Movie Recommendation/app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+movies_dict = pickle.load(open('moviesDict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        movie_id = i[0]
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+st.title("Movie Recommender System")
+
+selectedMovieName = st.selectbox(
+    'Enter the movie name',
+    movies['title'].values
+)
+
+if st.button("Recommend"):
+    recommendations = recommend(selectedMovieName)
+    for i in recommendations:
+        st.write(i)
